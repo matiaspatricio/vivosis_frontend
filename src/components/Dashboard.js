9ea9e5e81e498e75adcf0dc8aa6d893b03efda9c
matiaspatricio <matiasnrpatricio@gmail.com>
2024-06-30 23:42:19 -0300

    Actualizacion de 30/6 parte 1

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,47 +1,64 @@
 import { useEffect, useState } from "react";
 import { Typography, Card, CardContent, Grid } from "@mui/material";
-import { startOfToday, subDays, format } from 'date-fns';
-import { getPedidosPendientes, getPedidosMes, getPedidosSemana, getPedidosAyer , getPedidosHoy, getPedidosSemanaAnterior } from "./api/pedido/pedido";
+import { startOfToday, subDays, format } from "date-fns";
+import {
+  getPedidosPendientes,
+  getPedidosMes,
+  getPedidosSemana,
+  getPedidosAyer,
+  getPedidosHoy,
+  getPedidosSemanaAnterior,
+} from "./api/pedido/pedido";
 
 const Dashboard = () => {
   const [pedidosPendientes, setPedidosPendientes] = useState([]);
   const [pedidosPreparados, setPedidosPreparados] = useState([]);
-  const [clientesConPedidosPendientes, setClientesConPedidosPendientes] = useState([]);
-  const [clientesConPedidosPreparados, setClientesConPedidosPreparados] = useState([]);
-  const [pedidosHoy, setPedidosHoy] = useState([]); 
-  const [pedidosAyer, setPedidosAyer] = useState([]);
-  const [pedidosSemana, setPedidosSemana] = useState([]); 
-  const [pedidosSemanaAnterior, setPedidosSemanaAnterior] = useState([]); 
-  
+  const [clientesConPedidosPendientes, setClientesConPedidosPendientes] =
+    useState([]);
+  const [clientesConPedidosPreparados, setClientesConPedidosPreparados] =
+    useState([]);
   const [dineroPendiente, setDineroPendiente] = useState(0);
-  const [totalHoy, setTotalHoy] = useState(0);
-  const [totalSemana, setTotalSemana] = useState(0);
-  const [totalSemanaAnterior, setTotalSemanaAnterior] = useState(0);
-  
-  const [totalAyer, setTotalAyer] = useState(0);
+  const [totalesDashboard, setTotalesDashboard] = useState(0);
   const [montoTotal, setMontoTotal] = useState(0);
 
   useEffect(() => {
-    const usuario = localStorage.getItem('usuario');
+    const usuario = localStorage.getItem("usuario");
 
     const fetchData = async () => {
       try {
-        setPedidosHoy(await getPedidosHoy());
-        setPedidosAyer(await getPedidosAyer());
-        setPedidosSemana(await getPedidosSemana());
-        setPedidosSemanaAnterior(await getPedidosSemanaAnterior());
-        
         const pedidosMes = await getPedidosMes();
-        
-        setMontoTotal(pedidosMes.reduce((total, pedido) => total + pedido.total, 0));
+        const totalesDashboard = await getPedidosPendientes();
+        setTotalesDashboard(totalesDashboard);
+
+        setMontoTotal(
+          pedidosMes.reduce((total, pedido) => total + pedido.total, 0)
+        );
 
         const data = await getPedidosPendientes();
-        setPedidosPendientes(data.filter((pedido) => pedido.estado_pedido === "PENDIENTE"));
-        setPedidosPreparados(data.filter((pedido) => pedido.estado_pedido === "PREPARADO"));
-        setClientesConPedidosPendientes(Array.from(new Set(data.map((pedido) => pedido.nombre_cliente))));
-        setClientesConPedidosPreparados(Array.from(new Set(data.filter((pedido) => pedido.estado_pedido === "PREPARADO").map((pedido) => pedido.nombre_cliente))));
-        
-        setDineroPendiente(data.filter((pedido) => pedido.estado_pago === "PENDIENTE").reduce((total, pedido) => total + pedido.total, 0));
+        setPedidosPendientes(
+          data.filter((pedido) => pedido.estado_pedido === "PENDIENTE")
+        );
+        setPedidosPreparados(
+          data.filter((pedido) => pedido.estado_pedido === "PREPARADO")
+        );
+        setClientesConPedidosPendientes(
+          Array.from(new Set(data.map((pedido) => pedido.nombre_cliente)))
+        );
+        setClientesConPedidosPreparados(
+          Array.from(
+            new Set(
+              data
+                .filter((pedido) => pedido.estado_pedido === "PREPARADO")
+                .map((pedido) => pedido.nombre_cliente)
+            )
+          )
+        );
+
+        setDineroPendiente(
+          data
+            .filter((pedido) => pedido.estado_pago === "PENDIENTE")
+            .reduce((total, pedido) => total + pedido.total, 0)
+        );
       } catch (error) {
         console.log("Error al cargar los pedidos:", error);
       }
@@ -57,22 +74,6 @@ const Dashboard = () => {
     });
   };
 
-  useEffect(() => {
-    setTotalHoy(pedidosHoy.reduce((total, pedido) => total + pedido.total, 0));
-  }, [pedidosHoy]);
-
-  useEffect(() => {
-    setTotalAyer(pedidosAyer.reduce((total, pedido) => total + pedido.total, 0));
-  }, [pedidosAyer]);
-
-  useEffect(() => {
-    setTotalSemana(pedidosSemana.reduce((total, pedido) => total + pedido.total, 0));
-  }, [pedidosSemana]);
-
-  useEffect(() => {
-    setTotalSemanaAnterior(pedidosSemanaAnterior.reduce((total, pedido) => total + pedido.total, 0));
-  }, [pedidosSemanaAnterior]);
-
   return (
     <Grid container spacing={2} mt={5}>
       <Grid item xs={12} md={6}>
@@ -83,18 +84,20 @@ const Dashboard = () => {
             </Typography>
             <Typography variant="h4">{pedidosPendientes.length}</Typography>
           </CardContent>
-        </Card>        
-      </Grid>      
+        </Card>
+      </Grid>
       <Grid item xs={12} md={6}>
         <Card>
           <CardContent>
             <Typography variant="h6" gutterBottom>
               Clientes con pedidos pendientes
             </Typography>
-            <Typography variant="h4">{clientesConPedidosPendientes.length}</Typography>
+            <Typography variant="h4">
+              {clientesConPedidosPendientes.length}
+            </Typography>
           </CardContent>
         </Card>
-      </Grid>            
+      </Grid>
       <Grid item xs={12} md={6}>
         <Card>
           <CardContent>
@@ -111,17 +114,9 @@ const Dashboard = () => {
             <Typography variant="h6" gutterBottom>
               Clientes con pedidos preparados
             </Typography>
-            <Typography variant="h4">{clientesConPedidosPreparados.length}</Typography>
-          </CardContent>
-        </Card>
-      </Grid>
-      <Grid item xs={12} md={6}>  
-        <Card>
-          <CardContent>
-            <Typography variant="h6" gutterBottom>
-              Cant. Pedidos hoy 
+            <Typography variant="h4">
+              {clientesConPedidosPreparados.length}
             </Typography>
-            <Typography variant="h4">{pedidosHoy.length}</Typography>
           </CardContent>
         </Card>
       </Grid>
@@ -129,70 +124,94 @@ const Dashboard = () => {
         <Card>
           <CardContent>
             <Typography variant="h6" gutterBottom>
-              Total de hoy 
+              Cant. Pedidos hoy
             </Typography>
-            <Typography variant="h4">{formatToCurrency(totalHoy)}</Typography>
+            <Typography variant="h4">{totalesDashboard.cantidadDia}</Typography>
           </CardContent>
         </Card>
-      </Grid>        
+      </Grid>
+      <Grid item xs={12} md={6}>
+        <Card>
+          <CardContent>
+            <Typography variant="h6" gutterBottom>
+              Total de hoy
+            </Typography>
+            <Typography variant="h4">
+              {formatToCurrency(totalesDashboard.totalHoy)}
+            </Typography>
+          </CardContent>
+        </Card>
+      </Grid>
       <Grid item xs={12} md={6}>
         <Card>
           <CardContent>
             <Typography variant="h6" gutterBottom>
               Cant. Pedidos ayer
             </Typography>
-            <Typography variant="h4">{pedidosAyer.length}</Typography>
+            <Typography variant="h4">
+              {totalesDashboard.cantidadAyer}
+            </Typography>
           </CardContent>
         </Card>
-      </Grid>       
+      </Grid>
       <Grid item xs={12} md={6}>
         <Card>
           <CardContent>
             <Typography variant="h6" gutterBottom>
               Total de ayer
             </Typography>
-            <Typography variant="h4">{formatToCurrency(totalAyer)}</Typography>
+            <Typography variant="h4">
+              {formatToCurrency(totalesDashboard.totalAyer)}
+            </Typography>
           </CardContent>
         </Card>
-      </Grid> 
-      <Grid item xs={12} md={6}>        
+      </Grid>
+      <Grid item xs={12} md={6}>
         <Card>
           <CardContent>
             <Typography variant="h6" gutterBottom>
               Cant. esta semana
             </Typography>
-            <Typography variant="h4">{pedidosSemana.length}</Typography>
+            <Typography variant="h4">
+              {totalesDashboard.cantidadSemana}
+            </Typography>
           </CardContent>
         </Card>
       </Grid>
-      <Grid item xs={12} md={6}>        
+      <Grid item xs={12} md={6}>
         <Card>
           <CardContent>
             <Typography variant="h6" gutterBottom>
               Total esta semana
             </Typography>
-            <Typography variant="h4">{formatToCurrency(totalSemana)}</Typography>
+            <Typography variant="h4">
+              {formatToCurrency(totalesDashboard.totalSemana)}
+            </Typography>
           </CardContent>
         </Card>
       </Grid>
-      
-      <Grid item xs={12} md={6}>        
+
+      <Grid item xs={12} md={6}>
         <Card>
           <CardContent>
             <Typography variant="h6" gutterBottom>
-              Cant.  semana anterior
+              Cant. semana anterior
             </Typography>
-            <Typography variant="h4">{pedidosSemanaAnterior.length}</Typography>
+            <Typography variant="h4">
+              {totalesDashboard.cantidadSemanaAnterior}
+            </Typography>
           </CardContent>
         </Card>
       </Grid>
-      <Grid item xs={12} md={6}>        
+      <Grid item xs={12} md={6}>
         <Card>
           <CardContent>
             <Typography variant="h6" gutterBottom>
               Total semana anterior
             </Typography>
-            <Typography variant="h4">{formatToCurrency(totalSemanaAnterior)}</Typography>
+            <Typography variant="h4">
+              {formatToCurrency(totalesDashboard.totalSemanaAnterior)}
+            </Typography>
           </CardContent>
         </Card>
       </Grid>
@@ -203,18 +222,62 @@ const Dashboard = () => {
             <Typography variant="h6" gutterBottom>
               Dinero pendiente de cobro
             </Typography>
-            <Typography variant="h4">${dineroPendiente ? formatToCurrency(dineroPendiente) : 0}</Typography>
+            <Typography variant="h4">
+              ${dineroPendiente ? formatToCurrency(dineroPendiente) : 0}
+            </Typography>
           </CardContent>
         </Card>
       </Grid>
-      
+
       <Grid item xs={12} md={6}>
         <Card>
           <CardContent>
             <Typography variant="h6" gutterBottom>
-              Monto total de pedidos del mes
+            Cantidad pedidos mes              
             </Typography>
-            <Typography variant="h4">{montoTotal ? formatToCurrency(montoTotal) : 0 }</Typography>
+            <Typography variant="h4">
+              {totalesDashboard.totalMes
+                ? formatToCurrency(totalesDashboard.cantidadMes)
+                : 0}
+            </Typography>
+          </CardContent>
+        </Card>
+        <Card>
+          <CardContent>
+            <Typography variant="h6" gutterBottom>
+              
+            </Typography>
+            <Typography variant="h4">
+            Total de pedidos del mes
+                ? formatToCurrency(totalesDashboard.totalMes)
+                : 0}
+            </Typography>
+          </CardContent>
+        </Card>
+      </Grid>
+      <Grid item xs={12} md={6}>
+        <Card>
+          <CardContent>
+            <Typography variant="h6" gutterBottom>
+              Cantidad pedidos mes anterior
+            </Typography>
+            <Typography variant="h4">
+              {totalesDashboard.totalMes
+                ? formatToCurrency(totalesDashboard.cantidadMesAnterior)
+                : 0}
+            </Typography>
+          </CardContent>
+        </Card>
+        <Card>
+          <CardContent>
+            <Typography variant="h6" gutterBottom>
+              Total de pedidos del mes anterior
+            </Typography>
+            <Typography variant="h4">
+              {totalesDashboard.cantidadMes
+                ? formatToCurrency(totalesDashboard.totalMesAnterior)
+                : 0}
+            </Typography>
           </CardContent>
         </Card>
       </Grid>
--- src/components/api/pedido/pedido.js
+++ src/components/api/pedido/pedido.js
@@ -5,58 +5,36 @@ export const getPedidosPendientes = async () => {
   const data = await response.json();
   return data;
 };
+export const getTotalesDashboard = async () => {
+  const response = await fetch(`${API_URL}/gettotalesdashboard`);
+  const data = await response.json();
+  return data;
+}
 
-export const getPedidosAyer = async () => {
-    const response = await fetch(`${API_URL}/getpedidosayer`);
-    const data = await response.json();
-    return data;
-  };
-
-  export const getPedidosSemana = async () => {
-    const response = await fetch(`${API_URL}/getpedidossemana`);
-    const data = await response.json();
-    return data;
-  };
-  export const getPedidosSemanaAnterior = async () => {
-    const response = await fetch(`${API_URL}/getpedidossemanaanterior`);
-    const data = await response.json();
-    return data;
-  };
-  
-  export const getPedidosMes = async () => {
-    const response = await fetch(`${API_URL}/getpedidosmes`);
-    const data = await response.json();
-    return data;
-  };
-
-  export const getPedidosHoy = async () => {
-    const response = await fetch(`${API_URL}/getpedidoshoy`);
-    const data = await response.json();
-    return data;
-  }
-
-  export const getAllPedidos = async () => {
-    const response = await fetch(`${API_URL}/getallpedidos`);
-    const data = await response.json();
-    return data;
-  }
-  export const actualizarPedido = async (pedido) => {
-    try {
-      //console.log('Pedido a actualizar:', pedido);
-      const response = await fetch(`https://vivosis.vercel.app/api/pedido/${pedido._id}`, {
-        method: 'PUT',
+export const getAllPedidos = async () => {
+  const response = await fetch(`${API_URL}/getallpedidos`);
+  const data = await response.json();
+  return data;
+};
+export const actualizarPedido = async (pedido) => {
+  try {
+    //console.log('Pedido a actualizar:', pedido);
+    const response = await fetch(
+      `https://vivosis.vercel.app/api/pedido/${pedido._id}`,
+      {
+        method: "PUT",
         headers: {
-          'Content-Type': 'application/json',
+          "Content-Type": "application/json",
         },
         body: JSON.stringify(pedido),
-      });
-  
-      const data = await response.json();
+      }
+    );
 
-      return data;
-    } catch (error) {
-      console.log('Error al modificar el pedido:', error);
-      throw error; // Lanza el error para que pueda ser manejado por la función que llame a esta.
-    }
-  };
-  
+    const data = await response.json();
+
+    return data;
+  } catch (error) {
+    console.log("Error al modificar el pedido:", error);
+    throw error; // Lanza el error para que pueda ser manejado por la función que llame a esta.
+  }
+};
